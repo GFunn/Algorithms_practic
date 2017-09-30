34237a4805ef808d260faee9cc513c18d456b235
Fong <kirkgang@qq.com>
2017-09-30 23:58:19 +0800

    Add files via upload

--- rec_bi_search.py
+++ rec_bi_search.py
@@ -1,18 +1,19 @@
-def rec_bi_search(lists, item):
-    left = 0
-    right = len(lists) - 1
+def rec_bi_search(lists, item, left=0, right=None):
+
+    if right is None:
+        right = len(lists)
+
     mid = int((left + right) / 2)
+
     if lists[mid] == item:
-        return lists[mid]
+        return mid
     if lists[mid] < item:
-        lists = lists[mid + 1:]
-        return rec_bi_search(lists, item)
+        return rec_bi_search(lists, item, mid, right)
     else:
-        lists = lists[:mid]
-        return rec_bi_search(lists, item)
+        return rec_bi_search(lists, item, left, mid)
 
 
 li = eval(input('\nPlease input a list: '))
 item = int(input('\nPlease input search item: '))
 result = rec_bi_search(li, item)
-print('The index of item is: %d' % li.index(result))
+print('The index of item is: %d' % result)
